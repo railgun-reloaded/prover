518278cbac37e262b0fe2064bd51f5a4c207f90e
illusioncoder7 <shubh.tiwari512@gmail.com>
2025-12-11 12:47:24 +0545

    add generic inteface for poi and transaction circuits

--- src/base-prover.ts
+++ src/base-prover.ts
@@ -1,75 +1,28 @@
-import { curves, groth16 } from 'snarkjs'
-
-import { extractPublicInputsFromCircuitInputs, snarkJSToStandardProof, standardToSnarkJSInput, standardToSnarkJSProof, standardToSnarkJSPublicInputs } from './formatter'
-import type {
-  BaseProver,
-  CircuitInputs,
-  Proof,
-  ProverArtifacts,
-  PublicInputs,
-  VKey
-} from './types'
+import type { ProverArtifacts,Proof } from "./transaction-types";
 
 /**
- * Implementation of BaseProver for Railgun circuits
+ * Base Prover interface
  */
-// eslint-disable-next-line import-x/group-exports
-export class RailgunBaseProver implements BaseProver {
+export interface BaseProver<TCircuitInputs,TPublicInputs> {
+  artifacts:ProverArtifacts
   /**
-   * Create a Railgun transaction proof
-   * @param circuitInputs - Circuit inputs for generating proof
-   * @param artifacts - Circuit artifacts
-   * @returns Proof
+   * Generate a proof for given circuit inputs
+   * @param circuitInputs - The inputs to the circuit
+   * @param artifacts - The prover artifacts with vkey,zkey and wasm
+   * @returns Promise resolving to circuit inputs and generated proof
    */
-  async prove (circuitInputs: CircuitInputs, artifacts: ProverArtifacts): Promise<{ proof: Proof, publicInputs: PublicInputs }> {
-  // Format the inputs into snarkJS format
-    const snarkJSFormattedInputs = standardToSnarkJSInput(circuitInputs)
-
-    // Generate proof
-    const { proof } = await groth16.fullProve(snarkJSFormattedInputs, artifacts.wasm, artifacts.zkey)
-
-    // Standardize the proof
-    const standardProof = snarkJSToStandardProof(proof)
-
-    // Extract public inputs
-    const snarkJSFormattedPublicInputs = extractPublicInputsFromCircuitInputs(circuitInputs, standardProof)
-
-    // Create snarkJS proof
-    const snarkJSFormattedProof = standardToSnarkJSProof(standardProof)
-
-    // Ensure proof passes verification
-    groth16.verify(artifacts.vkey, snarkJSFormattedPublicInputs, snarkJSFormattedProof)
-
-    // Format to Uint8Array and return
-    return { proof: standardProof, publicInputs: snarkJSFormattedPublicInputs }
-  }
+  prove(
+    circuitInputs: TCircuitInputs,
+  ): Promise<{proof:Proof,publicInputs:TPublicInputs}>;
 
   /**
-   * Verify a Railgun transaction proof
-   * @param vkey - Circuit verifying key
-   * @param publicInputs - Proof public inputs
-   * @param proof - Snark proof
-   * @returns is proof valid
+   * Verify a proof against circuit inputs
+   * @param circuitInputs - The inputs to the circuit
+   * @param proof - The proof to verify
+   * @returns Promise resolving to verification result
    */
-  async verify (vkey: VKey, publicInputs: PublicInputs, proof: Proof): Promise<boolean> {
-    const snarkJSFormattedProof = standardToSnarkJSProof(proof)
-    const snarkJSFormattedPublicInputs = standardToSnarkJSPublicInputs(publicInputs)
-
-    return groth16.verify(vkey, snarkJSFormattedPublicInputs, snarkJSFormattedProof)
-  }
-
-  /**
-   * Cleanup snarkJS resources
-   * @returns Promise<void>
-   *
-   * https://github.com/iden3/snarkjs/issues/152
-   * https://github.com/iden3/snarkjs/issues/393
-   */
-  async cleanupSnarkJS (): Promise<void> {
-  // Initialize the curve object controlling wasm threads
-    const curve = await curves.getCurveFromName('bn128')
-
-    // Terminate threads
-    curve.terminate()
-  }
+  verify(
+    publicInputs: TPublicInputs,
+    proof: Proof
+  ): Promise<boolean>;
 }
