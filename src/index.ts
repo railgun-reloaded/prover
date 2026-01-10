98b9e0baa6af35ece6e31fc8e6cf16691715590c
illusioncoder7 <shubh.tiwari512@gmail.com>
2026-01-10 12:25:50 +0545

    add adapter exports

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,5 @@ export type {
 } from './poi/types'
 export { SnarkjsTransactionProver } from './transaction/prover'
 export { SnarkjsPoiProver } from './poi/prover'
+export { createGroth16ForEngine, createGroth16FromTransactionProver, createGroth16FromPOIProver } from './core/groth16-adapter'
+export type { Groth16Prover } from './core/groth16-adapter'
