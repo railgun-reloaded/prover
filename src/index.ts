28a18fcd6fe0bd6c91e1b7638166eadba5141894
illusioncoder7 <shubh.tiwari512@gmail.com>
2026-01-21 17:48:19 +0545

    add formatter exports

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ export type {
   POICircuitInputs,
   POIPublicInputs
 } from './poi/types'
+export {standardToSnarkJSInput as standardToSnarJSTransactionInput} from './transaction/formatter'
+export {standardToSnarkJSInput as standardToSnarkJSPOIInput} from './poi/formatter'
 export { SnarkjsTransactionProver } from './transaction/prover'
 export { SnarkjsPoiProver } from './poi/prover'
 export { createGroth16ForEngine, createGroth16FromTransactionProver, createGroth16FromPOIProver } from './core/groth16-adapter'
