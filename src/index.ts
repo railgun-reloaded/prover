01472edc722d5e82f9f6f253b6beb89e666b13af
illusioncoder7 <shubh.tiwari512@gmail.com>
2026-01-28 16:40:32 +0545

    add bytes conversion exports

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,26 @@ export type {
   ProverArtifacts,
   VKey,
 } from './transaction/types'
+
 export type {
   POICircuitInputs,
-  POIPublicInputs
+  POIPublicInputs,
+  POISnarkjsFormattedCircuitInputs,
 } from './poi/types'
-export {standardToSnarkJSInput as standardToSnarkJSTransactionInput} from './transaction/formatter'
-export {standardToSnarkJSInput as standardToSnarkJSPOIInput} from './poi/formatter'
+
+export { standardToSnarkJSInput as standardToSnarkJSTransactionInput } from './transaction/formatter'
+export { standardToSnarkJSInput as standardToSnarkJSPOIInput, snarkJSToStandardInput as snarkJSToStandardPOIInput } from './poi/formatter'
+
 export { SnarkjsTransactionProver } from './transaction/prover'
 export { SnarkjsPoiProver } from './poi/prover'
+
 export { createGroth16ForEngine, createGroth16FromTransactionProver, createGroth16FromPOIProver } from './core/groth16-adapter'
 export type { Groth16Prover } from './core/groth16-adapter'
+
+export {
+  hexStringToUint8Array,
+  uint8ArrayToHexString,
+  uint8ArrayToNumberString,
+  numberStringToUint8Array,
+  arrayToByteLength,
+} from './core/bytes'
